545e0cb8a86ce0fcbc0abe80c165af1520f3a6f2
Dan Smith <upthewaterspout@apache.org>
2016-01-07 16:32:36 -0800

    Revert "Turning channel into a skelelton"
    This reverts commit c7ca561ed7739e69af93277ffddacdd80cb22596.

--- gemfire-core/src/main/java/com/gemstone/gemfire/internal/cache/persistence/CipherRandomAccessFile.java
+++ gemfire-core/src/main/java/com/gemstone/gemfire/internal/cache/persistence/CipherRandomAccessFile.java
@@ -1,162 +1,216 @@
 package com.gemstone.gemfire.internal.cache.persistence;
 
+import java.io.File;
 import java.io.FileDescriptor;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
+import java.nio.channels.ClosedByInterruptException;
+import java.nio.channels.FileChannel;
 import java.nio.channels.SeekableByteChannel;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.ShortBufferException;
+
+import com.gemstone.gemfire.GemFireIOException;
 
 public class CipherRandomAccessFile implements RandomAccessFileInterface {
+  private static final int DEFAULT_BLOCK_SIZE = 512;
   private RandomAccessFileInterface raf;
   private final CipherFileChannelImpl channel;
+  private boolean isClosed;
+  private int blockSize;
+  private int cipherOutputSize;
+  private long position;
+  private long length;
   private UninterruptibleFileChannel fileChannel;
+  private final ByteBuffer encryptedBuffer;
+  private final ByteBuffer plainTextBuffer;
+  private long lastWritePosition;
+  private final ByteBuffer writeBuffer;
   private Cipher encrypt;
   private Cipher decrypt;
 
   public CipherRandomAccessFile(RandomAccessFileInterface raf, Cipher encrypt, Cipher decrypt) throws FileNotFoundException {
     this.encrypt = encrypt;
     this.decrypt = decrypt;
+    int blockSize = encrypt.getBlockSize();
+    this.blockSize = blockSize == 0 ? DEFAULT_BLOCK_SIZE : blockSize;
+    this.cipherOutputSize = encrypt.getOutputSize(blockSize);
     this.raf = raf;
     this.fileChannel = raf.getChannel();
     this.channel = new CipherFileChannelImpl();
+    this.encryptedBuffer = ByteBuffer.allocate(this.cipherOutputSize);
+    this.plainTextBuffer = ByteBuffer.allocate(this.blockSize);
+    this.writeBuffer = plainTextBuffer;
   }
 
-  @Override
   public UninterruptibleFileChannel getChannel() {
     return channel;
   }
-
-  @Override
-  public void close() throws IOException {
-    // TODO Auto-generated method stub
+  
+  public synchronized void close() throws IOException {
+    this.isClosed = true;
+    this.raf.close();
   }
 
-  @Override
-  public void setLength(long newLength) throws IOException {
-    // TODO Auto-generated method stub
+  public synchronized void setLength(long newLength) throws IOException {
+    this.raf.setLength(newLength);
+    
   }
 
-  @Override
-  public FileDescriptor getFD() throws IOException {
-    // TODO Auto-generated method stub
-    return null;
+  public synchronized FileDescriptor getFD() throws IOException {
+    return this.raf.getFD();
   }
 
-  @Override
-  public long getFilePointer() throws IOException {
-    // TODO Auto-generated method stub
-    return 0;
+  public synchronized long getFilePointer() throws IOException {
+    return this.position;
   }
 
-  @Override
-  public void seek(long readPosition) throws IOException {
-    // TODO Auto-generated method stub
+  public synchronized void seek(long readPosition) throws IOException {
+    this.position = readPosition;
+    
   }
 
-  @Override
-  public void readFully(byte[] valueBytes) throws IOException {
-    // TODO Auto-generated method stub
+  public synchronized void readFully(byte[] valueBytes) throws IOException {
+    this.readFully(valueBytes, 0, valueBytes.length);
+    
   }
 
-  @Override
-  public void readFully(byte[] valueBytes, int i, int valueLength)
-      throws IOException {
-    // TODO Auto-generated method stub
+  public synchronized void readFully(byte[] valueBytes, int i, int valueLength) throws IOException {
+    ByteBuffer buf = ByteBuffer.wrap(valueBytes);
+    buf.position(i);
+    buf.limit(valueLength);
+    int read = 0;
+    while(read != valueLength) {
+      read += read(buf);
+    }
+  }
+  
+  protected synchronized int read(ByteBuffer buf) throws IOException {
+    this.fileChannel.position(cipherBlock(this.position));
+    
+    long initialPosition = this.position;
+    while(buf.remaining() > 0) {
+      encryptedBuffer.clear();
+      plainTextBuffer.clear();
+      this.fileChannel.read(encryptedBuffer);
+      try {
+        decrypt.doFinal(encryptedBuffer, plainTextBuffer);
+      } catch (ShortBufferException | IllegalBlockSizeException
+          | BadPaddingException e) {
+        throw new GemFireIOException("Error decrypting data", e);
+      }
+
+      plainTextBuffer.flip();
+      plainTextBuffer.position(blockPosition(position));
+      int toTransfer = Math.min(buf.remaining(), plainTextBuffer.remaining());
+      plainTextBuffer.limit(plainTextBuffer.position() + toTransfer);
+      buf.put(plainTextBuffer);
+      position+= toTransfer;
+    }
+    
+    return (int) (this.position - initialPosition);
+  }
+  
+  public int write(ByteBuffer src) throws IOException {
+    throw new UnsupportedOperationException();
   }
 
-  @Override
-  public long length() throws IOException {
-    // TODO Auto-generated method stub
-    return 0;
+  private long cipherBlock(long position) {
+    return (position/blockSize) * cipherOutputSize;
+  }
+  
+  private int blockPosition(long position) {
+    return (int) (position % blockSize);
   }
 
-
+  public synchronized long length() throws IOException {
+    return this.length;
+  }
+  
+  
   private class CipherFileChannelImpl implements UninterruptibleFileChannel {
 
     @Override
-    public long write(ByteBuffer[] srcs, int offset, int length)
+    public long read(final ByteBuffer[] dsts, final int offset, final int length)
         throws IOException {
-      // TODO Auto-generated method stub
-      return 0;
+      long result = 0;
+      for(int i = offset; i < length; i++) {
+        result += read(dsts[i]);
+      }
+      return result;
     }
 
     @Override
-    public long write(ByteBuffer[] srcs) throws IOException {
-      // TODO Auto-generated method stub
-      return 0;
+    public long read(final ByteBuffer[] dsts) throws IOException {
+      return read(dsts, 0, dsts.length);
     }
 
     @Override
-    public int read(ByteBuffer dst) throws IOException {
-      // TODO Auto-generated method stub
-      return 0;
+    public long write(final ByteBuffer[] srcs, final int offset, final int length)
+        throws IOException {
+      long result = 0;
+      for(int i = offset; i < length; i++) {
+        result += write(srcs[i]);
+      }
+      return result;
     }
 
     @Override
-    public int write(ByteBuffer src) throws IOException {
-      // TODO Auto-generated method stub
-      return 0;
+    public long write(final ByteBuffer[] srcs) throws IOException {
+      return write(srcs, 0, srcs.length);
+    }
+
+    @Override
+    public int read(final ByteBuffer dst) throws IOException {
+      return CipherRandomAccessFile.this.read(dst);
+    }
+
+    @Override
+    public int write(final ByteBuffer src) throws IOException {
+      return CipherRandomAccessFile.this.write(src);
     }
 
     @Override
     public long position() throws IOException {
-      // TODO Auto-generated method stub
-      return 0;
+      return getFilePointer();
     }
 
     @Override
-    public SeekableByteChannel position(long newPosition) throws IOException {
-      // TODO Auto-generated method stub
-      return null;
+    public SeekableByteChannel position(final long newPosition) throws IOException {
+      seek(newPosition);
+      return this;
     }
 
     @Override
     public long size() throws IOException {
-      // TODO Auto-generated method stub
-      return 0;
+      return length;
     }
 
     @Override
-    public SeekableByteChannel truncate(long size) throws IOException {
-      // TODO Auto-generated method stub
-      return null;
-    }
-
-    @Override
-    public long read(ByteBuffer[] dsts, int offset, int length)
-        throws IOException {
-      // TODO Auto-generated method stub
-      return 0;
-    }
-
-    @Override
-    public long read(ByteBuffer[] dsts) throws IOException {
-      // TODO Auto-generated method stub
-      return 0;
+    public SeekableByteChannel truncate(final long size) throws IOException {
+      throw new UnsupportedOperationException();
     }
 
     @Override
     public boolean isOpen() {
-      // TODO Auto-generated method stub
-      return false;
+      return !isClosed;
     }
 
     @Override
     public void close() throws IOException {
-      // TODO Auto-generated method stub
-      
+      CipherRandomAccessFile.this.close();
     }
 
     @Override
-    public void force(boolean b) throws IOException {
-      // TODO Auto-generated method stub
-      
+    public void force(final boolean b) throws IOException {
+      throw new UnsupportedOperationException();
     }
     
-
-
   }
 
 
