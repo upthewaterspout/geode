c4cf1d77e329820e9bbf050842f258e0e4c2f7e3
David Anuta <david.r.anuta@gmail.com>
2017-06-08 18:00:36 -0700

    Added some java docs and cleaned up some code

--- geode-assembly/src/test/java/org/apache/geode/session/tests/Client.java
+++ geode-assembly/src/test/java/org/apache/geode/session/tests/Client.java
@@ -14,6 +14,7 @@
  */
 package org.apache.geode.session.tests;
 
+import org.apache.geode.modules.session.CommandServlet;
 import org.apache.geode.modules.session.QueryCommand;
 import org.apache.http.Header;
 import org.apache.http.StatusLine;
@@ -32,8 +33,20 @@ import org.apache.http.util.EntityUtils;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+/**
+ * A simple http client that talks to a server running the session-testing-war.
+ *
+ * This client sends commands to the {@link CommandServlet} over http to modify session properties
+ * and returns the results. The client has support for connecting to multiple servers and sending
+ * the session cookie returned by one server to other servers, to emulate the behavior of a client
+ * talking to the servers through a load balancer.
+ *
+ * The client currently only targets servers running on "localhost"
+ *
+ * To set the server this client is targeting, use {@link #setPort}.
+ */
 public class Client {
-  private String host = "localhost";
+  private static final String HOST = "localhost";
   private int port = 8080;
   private String cookie;
   private HttpContext context;
@@ -51,26 +64,133 @@ public class Client {
     cookie = null;
   }
 
+  /**
+   * Change the server that the client is targeting.
+   * @param port the port that the server is listening on
+   */
   public void setPort(int port) {
     this.port = port;
   }
 
-  public void resetURI() {
-    reqURIBuild.setHost(host + ":" + port);
+  /**
+   * Get the value of a session attribute on the server
+   */
+  public Response get(String key) throws IOException, URISyntaxException {
+    return get(key, true);
+  }
+
+  /**
+   * Get the value of a session attribute on the server
+   */
+  public Response get(String key, boolean storeRespCookie) throws IOException, URISyntaxException {
+    resetURI();
+    reqURIBuild.setParameter("cmd", QueryCommand.GET.name());
+    reqURIBuild.setParameter("param", key);
+
+    return doRequest(new HttpGet(reqURIBuild.build()), storeRespCookie);
+  }
+
+  /**
+   * Set the value of a session attribute on the server
+   */
+  public Response set(String key, String value) throws IOException, URISyntaxException {
+    return set(key, value, true);
+  }
+
+  /**
+   * Set the value of a session attribute on the server
+   */
+  public Response set(String key, String value, boolean storeRespCookie)
+      throws IOException, URISyntaxException {
+    resetURI();
+    reqURIBuild.setParameter("cmd", QueryCommand.SET.name());
+    reqURIBuild.setParameter("param", key);
+    reqURIBuild.setParameter("value", value);
+
+    return doRequest(new HttpGet(reqURIBuild.build()), storeRespCookie);
+  }
+
+  /**
+   * Remove the session attribute on the server
+   * @param key - the session attribute to remove
+   */
+  public Response remove(String key) throws IOException, URISyntaxException {
+    return remove(key, true);
+  }
+
+  /**
+   * Remove the session attribute on the server
+   * @param key - the session attribute to remove
+   */
+  public Response remove(String key, boolean storeRespCookie)
+      throws URISyntaxException, IOException {
+    resetURI();
+    reqURIBuild.setParameter("cmd", QueryCommand.REMOVE.name());
+    reqURIBuild.setParameter("param", key);
+
+    return doRequest(new HttpGet(reqURIBuild.build()), storeRespCookie);
+  }
+
+  /**
+   * Invalidate this clients session on the server
+   */
+  public Response invalidate() throws IOException, URISyntaxException {
+    return invalidate(true);
+  }
+
+  /**
+   * Invalidate this clients session on the server
+   */
+  public Response invalidate(boolean storeRespCookie) throws IOException, URISyntaxException {
+    resetURI();
+    reqURIBuild.setParameter("cmd", QueryCommand.INVALIDATE.name());
+    reqURIBuild.setParameter("param", "null");
+
+    return doRequest(new HttpGet(reqURIBuild.build()), storeRespCookie);
+  }
+
+  /**
+   * Set the maximum inactive interval for this client's session on the server.
+   *
+   * If this time interval elapses without activity on the session, the session
+   * will expire.
+   *
+   * @param time - Time in seconds until the session should expire
+   */
+  public Response setMaxInactive(int time) throws IOException, URISyntaxException {
+    return setMaxInactive(time, true);
+  }
+
+  /**
+   * Set the maximum inactive interval for this client's session on the server.
+   *
+   * If this time interval elapses without activity on the session, the session
+   * will expire.
+   *
+   * @param time - Time in seconds until the session should expire
+   */
+  public Response setMaxInactive(int time, boolean storeRespCookie)
+      throws IOException, URISyntaxException {
+    resetURI();
+    reqURIBuild.setParameter("cmd", QueryCommand.SET_MAX_INACTIVE.name());
+    reqURIBuild.setParameter("value", Integer.toString(time));
+
+    return doRequest(new HttpGet(reqURIBuild.build()), storeRespCookie);
+  }
+
+  private void resetURI() {
+    reqURIBuild.setHost(HOST + ":" + port);
     reqURIBuild.clearParameters();
   }
 
+  /**
+   * Sends a request to the server and returns a custom response
+   * object with the result
+   * @param storeRespCookie if true, retain the value of a "Set-Cookie" header
+   * returned in the response.
+   */
   private Response doRequest(HttpGet req, boolean storeRespCookie) throws IOException {
-    if (cookie != null) {
-      BasicClientCookie cookie = new BasicClientCookie("JSESSIONID", this.cookie);
-      cookie.setDomain(req.getURI().getHost());
-      cookie.setPath("/");
-
-      BasicCookieStore cookieStore = new BasicCookieStore();
-      cookieStore.addCookie(cookie);
-
-      context.setAttribute(HttpClientContext.COOKIE_STORE, cookieStore);
-    }
+    addCookieHeader(req);
 
     CloseableHttpResponse resp = httpclient.execute(req, context);
 
@@ -93,6 +213,20 @@ public class Client {
     return response;
   }
 
+  private void addCookieHeader(HttpGet req) {
+    //Set the cookie header
+    if (cookie != null) {
+      BasicClientCookie cookie = new BasicClientCookie("JSESSIONID", this.cookie);
+      cookie.setDomain(req.getURI().getHost());
+      cookie.setPath("/");
+
+      BasicCookieStore cookieStore = new BasicCookieStore();
+      cookieStore.addCookie(cookie);
+
+      context.setAttribute(HttpClientContext.COOKIE_STORE, cookieStore);
+    }
+  }
+
   private String getCookieHeader(CloseableHttpResponse resp) {
     Header lastHeader = resp.getLastHeader("Set-Cookie");
 
@@ -102,70 +236,12 @@ public class Client {
     return lastHeader.getElements()[0].getValue();
   }
 
-  public Response get(String key) throws IOException, URISyntaxException {
-    return get(key, true);
-  }
-
-  public Response get(String key, boolean storeRespCookie) throws IOException, URISyntaxException {
-    resetURI();
-    reqURIBuild.setParameter("cmd", QueryCommand.GET.name());
-    reqURIBuild.setParameter("param", key);
-
-    return doRequest(new HttpGet(reqURIBuild.build()), storeRespCookie);
-  }
-
-  public Response set(String key, String value) throws IOException, URISyntaxException {
-    return set(key, value, true);
-  }
-
-  public Response set(String key, String value, boolean storeRespCookie)
-      throws IOException, URISyntaxException {
-    resetURI();
-    reqURIBuild.setParameter("cmd", QueryCommand.SET.name());
-    reqURIBuild.setParameter("param", key);
-    reqURIBuild.setParameter("value", value);
-
-    return doRequest(new HttpGet(reqURIBuild.build()), storeRespCookie);
-  }
-
-  public Response remove(String key) throws IOException, URISyntaxException {
-    return remove(key, true);
-  }
-
-  public Response remove(String key, boolean storeRespCookie)
-      throws URISyntaxException, IOException {
-    resetURI();
-    reqURIBuild.setParameter("cmd", QueryCommand.REMOVE.name());
-    reqURIBuild.setParameter("param", key);
-
-    return doRequest(new HttpGet(reqURIBuild.build()), storeRespCookie);
-  }
-
-  public Response invalidate() throws IOException, URISyntaxException {
-    return invalidate(true);
-  }
-
-  public Response invalidate(boolean storeRespCookie) throws IOException, URISyntaxException {
-    resetURI();
-    reqURIBuild.setParameter("cmd", QueryCommand.INVALIDATE.name());
-    reqURIBuild.setParameter("param", "null");
-
-    return doRequest(new HttpGet(reqURIBuild.build()), storeRespCookie);
-  }
-
-  public Response setMaxInactive(int time) throws IOException, URISyntaxException {
-    return setMaxInactive(time, true);
-  }
-
-  public Response setMaxInactive(int time, boolean storeRespCookie)
-      throws IOException, URISyntaxException {
-    resetURI();
-    reqURIBuild.setParameter("cmd", QueryCommand.SET_MAX_INACTIVE.name());
-    reqURIBuild.setParameter("value", Integer.toString(time));
-
-    return doRequest(new HttpGet(reqURIBuild.build()), storeRespCookie);
-  }
-
+  /**
+   * A response received from the server.
+   *
+   * Currently contains the text value of the response body, as well
+   * as the session cookie.
+   */
   public class Response {
     private final String sessionCookie;
     private final String response;
@@ -177,14 +253,25 @@ public class Client {
       this.isNew = isNew;
     }
 
+    /**
+     * The session cookie associated with this client.
+     */
     public String getSessionCookie() {
       return sessionCookie;
     }
 
+    /**
+     *
+     * The String value of the response body.
+     */
     public String getResponse() {
       return response;
     }
 
+    /**
+     * @return true if this response contained a "Set-Cookie" header, indicating
+     * that the server is setting a new value for the session cookie.
+     */
     public boolean isNew() {
       return isNew;
     }
--- geode-assembly/src/test/java/org/apache/geode/session/tests/ContainerManager.java
+++ geode-assembly/src/test/java/org/apache/geode/session/tests/ContainerManager.java
@@ -31,6 +31,12 @@ import org.codehaus.cargo.generic.configuration.DefaultConfigurationFactory;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * Manages multiple J2EE containers using cargo.
+ *
+ * Provides methods to start and stop J2EE containers and obtain the http ports
+ * those containers are listening on.
+ */
 public class ContainerManager {
   private static final String PLAIN_LOG_FILE = "/tmp/logs/container";
 
@@ -42,7 +48,192 @@ public class ContainerManager {
     installs = new ArrayList<>();
   }
 
-  public InstalledLocalContainer addContainer(ContainerInstall install, int index)
+
+  /**
+   * Add a new container to manage using the specified installation
+   *
+   * The container will not be running until one of the start methods is called.
+   */
+  public InstalledLocalContainer addContainer(ContainerInstall install) throws IOException {
+    return addContainer(install, containers.size());
+  }
+
+  /**
+   * Add multiple containers to manage using the specified installation.
+   *
+   * The containers will not be running until one of the start methods is called.
+   */
+  public void addContainers(int numContainers, ContainerInstall install) throws IOException {
+    for (int i = 0; i < numContainers; i++)
+      addContainer(install);
+  }
+
+  /**
+   * Return the http port the given container is listening on, if the container is running
+   * @throws IllegalStateException if the container is not running.
+   */
+  public String getContainerPort(int index) {
+    LocalConfiguration config = getContainer(index).getConfiguration();
+    config.applyPortOffset();
+
+    if (!getContainer(index).getState().isStarted())
+    {
+      throw new IllegalStateException("Port has not yet been assigned to container " + index);
+    }
+    return config.getPropertyValue(ServletPropertySet.PORT);
+  }
+
+  /**
+   * @return the number of containers managed
+   */
+  public int numContainers() {
+    return containers.size();
+  }
+
+  public ArrayList<Integer> getContainerIndexesWithState(String state) {
+    ArrayList<Integer> indexes = new ArrayList<>();
+    for (int i = 0; i < numContainers(); i++) {
+      if (state.equals(State.STARTED.toString()) || state.equals(State.STOPPED.toString())
+          || state.equals(State.STARTED.toString()) || state.equals(State.STOPPING.toString())
+          || state.equals(State.UNKNOWN.toString())) {
+        if (getContainer(i).getState().toString().equals(state))
+          indexes.add(i);
+      } else
+        throw new IllegalArgumentException(
+            "State must be one of the 5 specified cargo state strings (stopped, started, starting, stopping, or unknown). Given: "
+                + state);
+    }
+    return indexes;
+  }
+
+  /**
+   * Return the cargo container of all of the containers in the given state
+   */
+  public ArrayList<InstalledLocalContainer> getContainersWithState(String state) {
+    ArrayList<InstalledLocalContainer> statedContainers = new ArrayList<>();
+    for (int index : getContainerIndexesWithState(state))
+      statedContainers.add(getContainer(index));
+    return statedContainers;
+  }
+
+  private ArrayList<Integer> getInactiveContainerIndexes() {
+    ArrayList<Integer> indexes = getContainerIndexesWithState(State.STOPPED.toString());
+    indexes.addAll(getContainerIndexesWithState(State.UNKNOWN.toString()));
+    return indexes;
+  }
+
+  private ArrayList<InstalledLocalContainer> getInactiveContainers() {
+    ArrayList<InstalledLocalContainer> inactiveContainers =
+        getContainersWithState(State.STOPPED.toString());
+    inactiveContainers.addAll(getContainersWithState(State.UNKNOWN.toString()));
+    return inactiveContainers;
+  }
+
+  private ArrayList<Integer> getActiveContainerIndexes() {
+    return getContainerIndexesWithState(State.STARTED.toString());
+  }
+
+  public ArrayList<InstalledLocalContainer> getActiveContainers() {
+    return getContainersWithState(State.STARTED.toString());
+  }
+
+  public InstalledLocalContainer getContainer(int index) {
+    return containers.get(index);
+  }
+
+  public ContainerInstall getContainerInstall(int index) {
+    return installs.get(index);
+  }
+
+  /**
+   * Get a textual description of the given container.
+   */
+  public String getContainerDescription(int index) {
+    String port = "<not started>";
+    try
+    {
+      port = String.valueOf(getContainerPort(index));
+    } catch (IllegalStateException ise) {}
+
+    return getContainerInstall(index).getContainerDescription() + "_" + index + ":" + port;
+  }
+
+  /**
+   * Start the given container
+   */
+  public void startContainer(int index) {
+    InstalledLocalContainer container = getContainer(index);
+    if (!container.getState().isStarted()) {
+      int[] ports = AvailablePortHelper.getRandomAvailableTCPPorts(3);
+      container.getConfiguration().setProperty(ServletPropertySet.PORT, Integer.toString(ports[0]));
+      container.getConfiguration().setProperty(GeneralPropertySet.RMI_PORT, Integer.toString(ports[1]));
+      container.getConfiguration().setProperty(TomcatPropertySet.AJP_PORT, Integer.toString(ports[2]));
+      container.getConfiguration().setProperty(GeneralPropertySet.PORT_OFFSET, "0");
+
+      container.start();
+      System.out.println("Started container " + getContainerDescription(index));
+    } else
+      throw new IllegalArgumentException("Cannot start container "
+          + getContainerDescription(index) + " it has currently " + container.getState());
+  }
+
+  /**
+   * Stop the given container
+   */
+  public void stopContainer(int index) {
+    InstalledLocalContainer container = getContainer(index);
+    if (container.getState().isStarted()) {
+      container.stop();
+      System.out.println("Stopped container" + index + " " + getContainerDescription(index));
+    } else
+      throw new IllegalArgumentException("Cannot stop container "
+          + getContainerDescription(index) + " it is currently " + container.getState());
+  }
+
+  public void startContainers(int[] indexes) {
+    for (int index : indexes)
+      startContainer(index);
+  }
+
+  public void startContainers(ArrayList<Integer> indexes) {
+    for (int index : indexes)
+      startContainer(index);
+  }
+
+  public void stopContainers(int[] indexes) {
+    for (int index : indexes)
+      stopContainer(index);
+  }
+
+  public void stopContainers(ArrayList<Integer> indexes) {
+    for (int index : indexes)
+      stopContainer(index);
+  }
+
+  /**
+   * Start all containers that are not currently running.
+   */
+  public void startAllInactiveContainers() {
+    startContainers(getInactiveContainerIndexes());
+  }
+
+  /**
+   * Stop all containers that are currently running.
+   */
+  public void stopAllActiveContainers() {
+    stopContainers(getActiveContainerIndexes());
+  }
+
+  public void removeContainer(int index) {
+    stopContainer(index);
+    containers.remove(index);
+    installs.remove(index);
+  }
+
+  /**
+   * Create a container to manage, given an installation.
+   */
+  private InstalledLocalContainer addContainer(ContainerInstall install, int index)
       throws IOException {
     // Create the Cargo Container instance wrapping our physical container
     LocalConfiguration configuration = (LocalConfiguration) new DefaultConfigurationFactory()
@@ -76,151 +267,5 @@ public class ContainerManager {
     System.out.println("Setup container " + getContainerDescription(index)
         + "\n-----------------------------------------");
     return container;
-  }
-
-  public InstalledLocalContainer addContainer(ContainerInstall install) throws IOException {
-    return addContainer(install, containers.size());
-  }
-
-  public void addContainers(int numContainers, ContainerInstall install) throws IOException {
-    for (int i = 0; i < numContainers; i++)
-      addContainer(install);
-  }
-
-  public String getContainerPort(int index) {
-    LocalConfiguration config = getContainer(index).getConfiguration();
-    config.applyPortOffset();
-
-    if (!getContainer(index).getState().isStarted())
-    {
-      throw new IllegalStateException("Port has not yet been assigned to container " + index);
-    }
-    return config.getPropertyValue(ServletPropertySet.PORT);
-  }
-
-  public int numContainers() {
-    return containers.size();
-  }
-
-  public ArrayList<Integer> getContainerIndexesWithState(String state) {
-    ArrayList<Integer> indexes = new ArrayList<>();
-    for (int i = 0; i < numContainers(); i++) {
-      if (state.equals(State.STARTED.toString()) || state.equals(State.STOPPED.toString())
-          || state.equals(State.STARTED.toString()) || state.equals(State.STOPPING.toString())
-          || state.equals(State.UNKNOWN.toString())) {
-        if (getContainer(i).getState().toString().equals(state))
-          indexes.add(i);
-      } else
-        throw new IllegalArgumentException(
-            "State must be one of the 5 specified cargo state strings (stopped, started, starting, stopping, or unknown). Given: "
-                + state);
-    }
-    return indexes;
-  }
-
-  public ArrayList<InstalledLocalContainer> getContainersWithState(String state) {
-    ArrayList<InstalledLocalContainer> statedContainers = new ArrayList<>();
-    for (int index : getContainerIndexesWithState(state))
-      statedContainers.add(getContainer(index));
-    return statedContainers;
-  }
-
-  public ArrayList<Integer> getInactiveContainerIndexes() {
-    ArrayList<Integer> indexes = getContainerIndexesWithState(State.STOPPED.toString());
-    indexes.addAll(getContainerIndexesWithState(State.UNKNOWN.toString()));
-    return indexes;
-  }
-
-  public ArrayList<InstalledLocalContainer> getInactiveContainers() {
-    ArrayList<InstalledLocalContainer> inactiveContainers =
-        getContainersWithState(State.STOPPED.toString());
-    inactiveContainers.addAll(getContainersWithState(State.UNKNOWN.toString()));
-    return inactiveContainers;
-  }
-
-  public ArrayList<Integer> getActiveContainerIndexes() {
-    return getContainerIndexesWithState(State.STARTED.toString());
-  }
-
-  public ArrayList<InstalledLocalContainer> getActiveContainers() {
-    return getContainersWithState(State.STARTED.toString());
-  }
-
-  public InstalledLocalContainer getContainer(int index) {
-    return containers.get(index);
-  }
-
-  public ContainerInstall getContainerInstall(int index) {
-    return installs.get(index);
-  }
-
-  public String getContainerDescription(int index) {
-    String port = "<not started>";
-    try
-    {
-      port = String.valueOf(getContainerPort(index));
-    } catch (IllegalStateException ise) {}
-
-    return getContainerInstall(index).getContainerDescription() + "_" + index + ":" + port;
-  }
-
-  public void startContainer(int index) {
-    InstalledLocalContainer container = getContainer(index);
-    if (!container.getState().isStarted()) {
-      int[] ports = AvailablePortHelper.getRandomAvailableTCPPorts(3);
-      container.getConfiguration().setProperty(ServletPropertySet.PORT, Integer.toString(ports[0]));
-      container.getConfiguration().setProperty(GeneralPropertySet.RMI_PORT, Integer.toString(ports[1]));
-      container.getConfiguration().setProperty(TomcatPropertySet.AJP_PORT, Integer.toString(ports[2]));
-      container.getConfiguration().setProperty(GeneralPropertySet.PORT_OFFSET, "0");
-
-      container.start();
-      System.out.println("Started container " + getContainerDescription(index));
-    } else
-      throw new IllegalArgumentException("Cannot start container "
-          + getContainerDescription(index) + " it has currently " + container.getState());
-  }
-
-  public void stopContainer(int index) {
-    InstalledLocalContainer container = getContainer(index);
-    if (container.getState().isStarted()) {
-      container.stop();
-      System.out.println("Stopped container" + index + " " + getContainerDescription(index));
-    } else
-      throw new IllegalArgumentException("Cannot stop container "
-          + getContainerDescription(index) + " it is currently " + container.getState());
-  }
-
-  public void startContainers(int[] indexes) {
-    for (int index : indexes)
-      startContainer(index);
-  }
-
-  public void startContainers(ArrayList<Integer> indexes) {
-    for (int index : indexes)
-      startContainer(index);
-  }
-
-  public void stopContainers(int[] indexes) {
-    for (int index : indexes)
-      stopContainer(index);
-  }
-
-  public void stopContainers(ArrayList<Integer> indexes) {
-    for (int index : indexes)
-      stopContainer(index);
-  }
-
-  public void startAllInactiveContainers() {
-    startContainers(getInactiveContainerIndexes());
-  }
-
-  public void stopAllActiveContainers() {
-    stopContainers(getActiveContainerIndexes());
-  }
-
-  public void removeContainer(int index) {
-    stopContainer(index);
-    containers.remove(index);
-    installs.remove(index);
   }
 }
