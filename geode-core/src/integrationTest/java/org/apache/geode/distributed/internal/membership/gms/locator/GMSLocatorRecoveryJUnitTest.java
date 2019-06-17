4ed8b1dd3d0bb684509bfd115f811e5a93453f7d
Dan Smith <upthewaterspout@apache.org>
2019-06-17 12:09:52 -0700

    GEODE-6883: Removing some dependencies on core from membership
    * Removing some direct dependencies on classes outside of membership.
    * Replaced the pattern having each Service implement MessageHandler and
      have a single processMessage method with a switch statement. Instead
      we install individual MessageHandlers for each message.
    * Removed GMSAuthenticator from Services. Instead we inject the
      Authenticator when we create a MembershipManager.
    
    Key-Gripped-By: Bill Burcham <bburcham@pivotal.io>
    Co-Authored-By: Bill Burcham <bburcham@pivotal.io>
    Co-Authored-By: Ernest Burghardt <eburghardt@pivotal.io>

--- geode-core/src/integrationTest/java/org/apache/geode/distributed/internal/membership/gms/locator/GMSLocatorRecoveryJUnitTest.java
+++ geode-core/src/integrationTest/java/org/apache/geode/distributed/internal/membership/gms/locator/GMSLocatorRecoveryJUnitTest.java
@@ -51,6 +51,7 @@ import org.apache.geode.distributed.internal.membership.DistributedMembershipLis
 import org.apache.geode.distributed.internal.membership.MemberFactory;
 import org.apache.geode.distributed.internal.membership.MembershipManager;
 import org.apache.geode.distributed.internal.membership.NetView;
+import org.apache.geode.distributed.internal.membership.adapter.auth.GMSAuthenticator;
 import org.apache.geode.internal.AvailablePortHelper;
 import org.apache.geode.internal.Version;
 import org.apache.geode.internal.admin.remote.RemoteTransportConfig;
@@ -180,7 +181,9 @@ public class GMSLocatorRecoveryJUnitTest {
       when(mockSystem.getConfig()).thenReturn(config);
       DMStats stats1 = mock(DMStats.class);
       m1 = MemberFactory.newMembershipManager(listener1, mockSystem, transport, stats1,
-          SecurityServiceFactory.create());
+          SecurityServiceFactory.create(),
+          new GMSAuthenticator(mockSystem.getSecurityProperties(), mockSystem.getSecurityService(),
+              mockSystem.getSecurityLogWriter(), mockSystem.getInternalLogWriter()));
 
       GMSLocator l2 = new GMSLocator(SocketCreator.getLocalHost(),
           m1.getLocalMember().getHost() + "[" + port + "]", true, true, new LocatorStats(), "");
