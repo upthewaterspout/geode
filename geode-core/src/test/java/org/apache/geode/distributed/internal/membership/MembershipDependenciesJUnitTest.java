277e5f3b209a1612a9508dea8d5cba5c304172da
Dan Smith <upthewaterspout@apache.org>
2019-06-18 16:48:49 -0700

    Adding some exceptions to our dependency test
    Starting the process of adding exceptions to our test of dependencies.
    These exceptions can become a more concise list of what we need to fix
    before membership can become its own module.

--- geode-core/src/test/java/org/apache/geode/distributed/internal/membership/MembershipDependenciesJUnitTest.java
+++ geode-core/src/test/java/org/apache/geode/distributed/internal/membership/MembershipDependenciesJUnitTest.java
@@ -1,7 +1,9 @@
 package org.apache.geode.distributed.internal.membership;
 
 import static com.tngtech.archunit.base.DescribedPredicate.not;
+import static com.tngtech.archunit.core.domain.JavaClass.Predicates.assignableTo;
 import static com.tngtech.archunit.core.domain.JavaClass.Predicates.resideInAPackage;
+import static com.tngtech.archunit.core.domain.JavaClass.Predicates.type;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
 import com.tngtech.archunit.junit.AnalyzeClasses;
@@ -11,22 +13,60 @@ import com.tngtech.archunit.lang.ArchRule;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
+import org.apache.geode.DataSerializer;
+import org.apache.geode.GemFireException;
+import org.apache.geode.distributed.internal.DMStats;
+import org.apache.geode.distributed.internal.DistributionConfig;
+import org.apache.geode.distributed.internal.DistributionMessage;
+import org.apache.geode.internal.DataSerializableFixedID;
+import org.apache.geode.internal.logging.LogService;
+
 /*
- Run manually for now.
- TODO: stop ignoring once we've eliminated all the violations yeah!
+ * Run manually for now.
+ * TODO: stop ignoring once we've eliminated all the violations yeah!
  */
 @Ignore("Ignoring until all membership dependencies are cleaned up")
 @RunWith(ArchUnitRunner.class)
-@AnalyzeClasses(packages="org.apache.geode.distributed.internal.membership..")
+@AnalyzeClasses(packages = "org.apache.geode.distributed.internal.membership..")
 public class MembershipDependenciesJUnitTest {
 
   @ArchTest
   public static final ArchRule membershipDoesntDependOnCore = classes()
       .that()
       .resideInAPackage("org.apache.geode.distributed.internal.membership..")
+      .and()
+      .resideOutsideOfPackage("org.apache.geode.distributed.internal.membership.adapter..")
+      .and()
+
+      // GEODE-XXX: InternalDistributedMember needs to move out of the package
+      .areNotAssignableFrom(InternalDistributedMember.class)
+
+
       .should()
       .onlyDependOnClassesThat(
           resideInAPackage("org.apache.geode.distributed.internal.membership..")
-              .or(not(resideInAPackage("org.apache.geode.."))));
+
+              .or(not(resideInAPackage("org.apache.geode..")))
+
+              // GEODE-XXX: Create a new stats interface for membership
+              .or(assignableTo(DMStats.class))
+
+              // GEODE-XXX: Figure out what to do with exceptions
+              .or(assignableTo(GemFireException.class))
+
+              // GEODE-XXX: Serialization needs to become its own module
+              .or(type(DataSerializer.class))
+              .or(type(DataSerializableFixedID.class))
+
+              // GEODE-XXX: Figure out what to do with messaging
+              .or(assignableTo(DistributionMessage.class))
+
+              // GEODE-XXX: Membership needs its own config object
+              .or(type(DistributionConfig.class))
+
+              // GEODE-XXX: Break depedency on geode logger
+              .or(type(LogService.class))
+
+  );
 
 }
