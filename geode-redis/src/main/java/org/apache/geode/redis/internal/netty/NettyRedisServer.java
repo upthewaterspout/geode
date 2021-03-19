fc5d9749bc3df0c969c4e115917cd76b66d105fb
Jacob Barrett <jbarrett@pivotal.io>
2021-03-19 09:14:10 -0700

    Some netty options.

--- geode-redis/src/main/java/org/apache/geode/redis/internal/netty/NettyRedisServer.java
+++ geode-redis/src/main/java/org/apache/geode/redis/internal/netty/NettyRedisServer.java
@@ -40,6 +40,7 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.EventLoopGroup;
+import io.netty.channel.epoll.EpollServerSocketChannel;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -126,7 +127,8 @@ public class NettyRedisServer {
             .channel(NioServerSocketChannel.class)
             .childHandler(createChannelInitializer())
             .option(ChannelOption.SO_REUSEADDR, true)
-            .option(ChannelOption.SO_RCVBUF, getBufferSize())
+            .childOption(ChannelOption.SO_SNDBUF, 1024 * 1024)
+            .childOption(ChannelOption.SO_RCVBUF, 32 * 1024)
             .childOption(ChannelOption.SO_KEEPALIVE, true)
             .childOption(ChannelOption.CONNECT_TIMEOUT_MILLIS, CONNECT_TIMEOUT_MILLIS)
             .childOption(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT);
